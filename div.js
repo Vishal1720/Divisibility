fa888d71aa453d06e1833dc87e376b0ff53bd17e
Vishal1720 <vishal198shetty@gmail.com>
2022-10-08 17:16:37 +0530

    limits to PREV and NEXT button from 1-10000 and the buttons turn transparent when they cannot be used

--- div.js
+++ div.js
@@ -1,13 +1,17 @@
 let a=document.getElementsByClassName("numbers");
 textEntered =document.getElementById("textField");
+nextBtn = document.getElementById("NEXT");
+prevBtn = document.getElementById("PREV");
 
-
+console.log(nextBtn)
 
 
 function checkIfDivisible()
 {
+  checkColor();
   let init=a[0].value-1;//alteration made here to support next 100 function
-   
+  if(textEntered.value != "")
+  {
     for(i=0;i<a.length;i++)
     {
        
@@ -21,7 +25,11 @@ function checkIfDivisible()
         a[i].style.backgroundColor = "orangered";
         init++;
     }
-    
+  } 
+  else
+  {
+    alert("Enter a number before submitting");
+  }
 
 };
 
@@ -55,7 +63,6 @@ for(i=0;i<a.length;i++)
 function prev100()
 {
     let base = a[0].value-1;
-    console.log("base is "+base);
     if(base>1)
     {
            for(i=a.length-1;i>=0;i--)//i=a.length-1 because it returns 100 and array starts from 0 and ends at n-1 
@@ -68,3 +75,33 @@ function prev100()
               checkIfDivisible();
     }
 }
+function checkColor()
+{
+  if(a[0].value == 1)//prev btn function can't be used when no is 1 because negative numbers will start
+  {
+  prevBtn.style.color = "transparent";
+  prevBtn.style.backgroundColor = "transparent";
+  prevBtn.style.borderColor = "transparent";
+  }
+  else
+  {
+    prevBtn.style.color = "whitesmoke";
+  prevBtn.style.backgroundColor = "blue";
+  prevBtn.style.borderColor = "black";
+  }
+
+  if(a[99].value == 10000)//max limit for next100 funcion
+  {
+    nextBtn.style.color = "transparent";
+    nextBtn.style.backgroundColor = "transparent";
+    nextBtn.style.borderColor = "transparent";
+    }
+    else
+    {
+      nextBtn.style.color = "whitesmoke";
+    nextBtn.style.backgroundColor = "blue";
+    nextBtn.style.borderColor = "black";
+    }
+
+}
+
